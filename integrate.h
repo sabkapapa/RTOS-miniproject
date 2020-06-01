8f49b7b2519ad7a57a430c1f57c2545323b525db
sabkapapa <42075338+sabkapapa@users.noreply.github.com>
2020-06-01 23:08:15 +0530

    Add files via upload

--- integrate.h
+++ integrate.h
@@ -1,34 +1,47 @@
 #pragma once
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+
 #include <pulse/simple.h>
 #include <pulse/error.h>
 #include <pulse/gccmacro.h>
 extern int DEBUG;
 #define BUFSIZE 1024
+#define VOICE_MESSAGE_SIZE 1024
+// struct for requests to join group.
+// used by client to tell the server which group it wants to join.
 
+
+struct JoinRequest {
+    char groupName[20];
+    char name[20];
+};
+// once the server accepts the client's request, it returns with the
+// id of the client and the group id it assigned to it.
+struct JoinResponse {
+    int id;
+    int groupId;
+};
+// once groups are assigned, there is normal chat exchange happening.
+// this is done by sending the following msg sequence.
+//for text send_voice_messages
+struct Message {
+    int id;
+    int groupId;
+    char name[20];
+    char message[200];
+};
+struct VoiceMessage {
+    int id;
+    int groupId;
+    char message[VOICE_MESSAGE_SIZE];
+};
 
 //for text chat
 struct sockaddr_in serv; // socket variable
-int fd; // file descripter
-int conn; // connection descripter
-char message[100] = "";
-void* send_func(void* n){
-  int sd = *(int*)n;
-  char msg[1024] = {0};
-  fgets(msg,1024,stdin);
-  send(sd,msg, strlen(msg),0);
-  return 0;
-}
 
-void* recv_func(void* n){
-  int rsd = *(int*)n;
-  char rply[1024];
-  read(rsd,rply,sizeof(rply));
-  printf("%s\n",rply );
-  return 0;
-}
 struct Connection{
   int sd;
   char* clientName;
@@ -38,26 +51,44 @@ struct Connection{
         fprintf(stderr, "%s (%s:%d)\n", msg, __FILE__, __LINE__); \
         exit(EXIT_FAILURE);                                       \
     }
-    static ssize_t loop_write(int fd, const void*data, size_t size) {
-        ssize_t ret = 0;
+static ssize_t loop_write(int fd, const void*data, size_t size) {
+    ssize_t ret = 0;
 
-        while (size > 0) {
-            ssize_t r;
+    while (size > 0) {
+        ssize_t r;
 
-            if ((r = write(fd, data, size)) < 0)
-                return r;
+        if ((r = write(fd, data, size)) < 0)
+            return r;
 
-            if (r == 0)
-                break;
+        if (r == 0)
+            break;
 
-            ret += r;
-            data = (const uint8_t*) data + r;
-            size -= (size_t) r;
-        }
-
-        return ret;
+        ret += r;
+        data = (const uint8_t*) data + r;
+        size -= (size_t) r;
     }
 
+    return ret;
+}
+static ssize_t loop_read(int fd, void* data, size_t size) {
+    ssize_t ret = 0;
+    while (size > 0) {
+        ssize_t r;
+        if ((r = read(fd, data, size)) < 0) return r;
+        if (r == 0) break;
+        ret += r;
+        data = (char*)data + r;
+        size -= (size_t)r;
+    }
+    return ret;
+}
+int send_data(int fd, const void* data, size_t size) {
+    return loop_write(fd, data, size);
+}
+
+int read_data(int fd, void* data, size_t size) {
+    return loop_read(fd, data, size);
+}
 /* The Sample format to use */
 static const pa_sample_spec ss = {
     .format = PA_SAMPLE_S16LE, .rate = 40100, .channels = 2};
